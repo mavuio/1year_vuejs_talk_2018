8b61b6e0e2c8d4019475ab279dc790aef6fafb9a
Manfred Wuits <manfred@monochrom.at>
2018-02-21 12:07:31 +0100

    autocommit

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp'),
-  livereload = require('gulp-livereload');
+livereload = require('gulp-livereload');
+var exec = require('child_process').exec;
 
 gulp.task('default', function() {
   livereload.listen();
   gulp.watch('PITCHME.md', function() {
+
+
+    exec('git add .; git commit -am "autocommit"; git push', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+
     livereload.reload();
     console.log('changed !')
   });
